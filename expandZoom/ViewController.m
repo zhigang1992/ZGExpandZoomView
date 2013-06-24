3441cd465067d552478027686bb35cb1bd7ec110
Kyle Fang <zhigang1992@gmail.com>
2013-06-24 22:41:57 +0800

    Added Expand Zoom View.

--- expandZoom/ViewController.m
+++ expandZoom/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
 
+static CGFloat kImageOriginHight = 240.f;
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *expandZoomImageView;
 @end
 
 @implementation ViewController
@@ -18,6 +21,25 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(kImageOriginHight, 0, 0, 0);
+    [self.tableView addSubview:self.expandZoomImageView];
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    self.expandZoomImageView.frame = CGRectMake(0, -kImageOriginHight, self.tableView.frame.size.width, kImageOriginHight);
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    CGFloat yOffset  = scrollView.contentOffset.y;
+    if (yOffset < -kImageOriginHight) {
+        CGRect f = self.expandZoomImageView.frame;
+        f.origin.y = yOffset;
+        f.size.height =  -yOffset;
+        self.expandZoomImageView.frame = f;
+    }
 }
 
 - (void)didReceiveMemoryWarning
